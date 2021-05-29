dd222e3bda1af6128be186ff1260be176af29a79
erfanaseri <erfanaseri@gmail.com>
2021-05-29 01:26:19 -0400

    main functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,18 @@
+import { func } from "assert-plus";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-console.log("just checking");
+
+const questions = document.querySelectorAll(".question");
+const answers = document.querySelectorAll(".answer");
+
+questions.forEach((questionEl) => {
+  questionEl.addEventListener("click", function (e) {
+    const sibling = questionEl.nextElementSibling;
+    this.classList.toggle("arrow-transform");
+    sibling.classList.toggle("content-hide");
+  });
+  //   if (sibling.classList.contains("content-hide")) {
+  //     questionEl.classList.add("divider");
+  //     //   questionEl.classList.remove("divider");
+  //   } else sibling.classList.add("divider");
+});
