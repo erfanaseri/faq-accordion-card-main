bc26cb6a7025ca29620273933a23a9cc50171c82
erfanaseri <erfanaseri@gmail.com>
2021-06-01 22:02:56 -0400

    revised

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,60 @@
 import { func } from "assert-plus";
+import { reset } from "colorette";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
 const questions = document.querySelectorAll(".question");
-const answers = document.querySelectorAll(".answer");
+const answersNode = document.querySelectorAll(".answer");
 
-questions.forEach((questionEl) => {
-  questionEl.addEventListener("click", function (e) {
-    // Changing the orientation of the arrow
-    this.classList.toggle("arrow-transform");
+// Converting the nodeList to an array so that we can loop over it later
+const answers = Array.from(answersNode);
 
-    // Adding the highlight class
-    this.classList.toggle("clicked-highlight");
+// Assigning a click button on each question element
+questions.forEach((question) => {
+  question.addEventListener("click", function (e) {
+    // Defining the sibling element each question clicked
+    const sibling = this.nextElementSibling;
 
-    // Toggle the answer upon each click
-    const sibling = questionEl.nextElementSibling;
+    if (sibling.classList.contains("content-hide")) {
+      resetElAnswer();
+      resetElQuestion();
+    }
+
     sibling.classList.toggle("content-hide");
+    this.classList.toggle("arrow-transform");
+    this.classList.toggle("clicked-highlight");
+    this.style.setProperty("border-bottom", "1px solid var(--clr-primary-7)");
 
-    // sibling.style.setProperty("transition", "display 1s ease-in");
+    if (!sibling.classList.contains("content-hide")) {
+      resetElQuestionDivider();
 
-    // Removing the divider line when there is the answer class
-    if (!sibling.classList.contains("content-hide"))
       this.style.setProperty("border-bottom", "initial");
-    else
-      this.style.setProperty("border-bottom", "1px solid var(--clr-primary-7)");
+      sibling.style.setProperty(
+        "border-bottom",
+        "1px solid var(--clr-primary-7)"
+      );
+    }
   });
 });
+
+const resetElAnswer = function () {
+  answers.forEach((answer) => {
+    answer.classList.add("content-hide");
+  });
+};
+
+const resetElQuestion = function () {
+  questions.forEach((question) => {
+    question.classList.remove("arrow-transform");
+    question.classList.remove("clicked-highlight");
+  });
+};
+
+const resetElQuestionDivider = function () {
+  questions.forEach((question) => {
+    question.style.setProperty(
+      "border-bottom",
+      "1px solid var(--clr-primary-7)"
+    );
+  });
+};
